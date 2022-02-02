ac407c9b717f816e9d21c37481fba942782071fb
Lyz03 <lizoe@netcourrier.com>
2022-02-02 10:18:42 +0100

    exo 178

--- classes/Personnage.php
+++ classes/Personnage.php
@@ -5,13 +5,13 @@ class Personnage {
 
     // La propriété id est privée, je ne peux pas la modifier ou la lire directement en dehors de cette classe
     private $id;
-    public $nom;
+    private $nom;
     public $x;
     public $y;
 
     // Constructeur de ma classe permettant de définir des valeurs par défaut lorsqu'elle est instanciée
     public function __construct() {
-        $this->nom = "Nom par défaut";
+        $this->setNom("Nom par défaut");
         $this->x = 0;
         $this->y = 0;
     }
@@ -20,6 +20,37 @@ class Personnage {
     public function walkRight() {
         $this->x += 1;
 
+    }
+
+    //  Méthode permettant de déplacer le personnage vers la gauche
+    public function walkLeft() {
+        $this->x -= 1;
+    }
+
+    // Méthode permettant de déplacer le personnage vers le haut
+    public function walkTop() {
+        $this->y -= 1;
+    }
+
+    // Méthode permettant de déplacer le personnage vers le bas
+    public function walkBottom() {
+        $this->y += 1;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
     }
 
     // Setter permettant de définir l'attribut privé id
